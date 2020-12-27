95cc5864799333bec5a769a71c78000b6b06c8de
Iqbal <hosiqb@gmail.com>
2020-12-28 00:23:04 +0600

    My First Commit

---
+++ resources/views/livewire/checkout-component.blade.php
@@ -0,0 +1,3 @@
+<div>
+    <h1>Check Out Page</h1>
+</div>
---
+++ routes/web.php
@@ -0,0 +1,33 @@
+<?php
+
+use App\Http\Livewire\CartComponent;
+use App\Http\Livewire\CheckoutComponent;
+use App\Http\Livewire\ContactComponentt;
+use App\Http\Livewire\HomeComponent;
+use App\Http\Livewire\ShopComponent;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', HomeComponent::class)->name('home');
+Route::get('/shop', ShopComponent::class)->name('shop');
+Route::get('/cart', CartComponent::class)->name('cart');
+Route::get('/checkout', CheckoutComponent::class)->name('checkout');
+Route::get('/contact', ContactComponentt::class)->name('contact');
+
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
